a2c02134c97e4a26f3f9d35d67ed060f45d2df3d
Arthur Gorjux <arthurgorjux@gmail.com>
2014-11-17 11:36:55 +0100

    Add some stuff

--- base.js
+++ base.js
@@ -20,8 +20,50 @@ function showstorage() {
 	$(".modal").fadeIn("fast");
 }
 
+function showcredits() {
+	closemessage();
+	makealert("credits","Règles du jeu","<div style='max-height:300px; overflow-y:auto;'><br>Blabla blabla</div>",true)
+}
+
+function startgame(){
+	closemessage();
+	makealert('startgame', 'Début du jeu !', 'Bienvenue sur le jeu d\'Arthur Gorjux<br/>Pour voir les règles, cliquez sur le bouton en bas à droite de la fenêtre', true);
+}
+
+function checkthings() {
+	/*updategold();
+	checkbuilding();
+	updateitems();
+	checkitem();
+	updatestatus();*/
+	//Update coins
+	if(coins == 1){
+		$('#gold-bar').html("");
+		$('#gold-bar').html(coins + ' pièce');
+	}else{
+		$('#gold-bar').html("");
+		$('#gold-bar').html(coins + ' pièces');
+	}
+	
+}
+
 $(document).ready(function(){
 	//initialisation des parametres
+	startgame();
+	coins = 0;
+	coins_per_second = 1;
 
-	
+	// Ajout de piece toutes les secondes
+	setInterval(function() {
+		coins += coins_per_second;
+		checkthings();
+	},1000);
+
+	//items
+	var items = [];
+
+	$("#home").click(function() {
+		closemessage();
+		makealert('home', 'Clique maison', 'Maison', true);
+	});
 });
